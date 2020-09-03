6bfdf106dde03b08ea680cf9aadb871efc5ad364
Arthur Bomfim Neto <you@example.com>
2020-09-03 18:04:12 -0300

    Exercicios gerais em Python

---
+++ exe9.py
@@ -0,0 +1,10 @@
+from math import radians,cos,tan,sin
+an = float(input('Digite o angulo:'))
+seno = sin(radians(an))
+print('O angulo de {} tem o seno de {:.2f}'.format(an , seno))
+cosseno = cos(radians(an))
+print('o angulu de {} tem o coseno de {:.2f} '.format(an,cosseno))
+tangente= tan(radians(an))
+print('o angulo {} tem a tengente de {:.2f} '.format(an,tangente))
+
+
